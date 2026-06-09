1acca1b5defe224576843359ce6859ae4b3e352f
M78Coder <leonm78coder@gmail.com>
2026-06-09 13:50:40 +0900

    .

--- GAPS_Project/nakagami/bash/run_preprocess_40M.sh
+++ GAPS_Project/nakagami/bash/run_preprocess_40M.sh
@@ -3,7 +3,8 @@
 # 実行方法: bash nakagami/bash/run_preprocess_40M.sh
 
 CSV_DIR=/mnt/ynakagami3/SimulationData/211209_isot_0205_renewal_looseTrigger_40M/csvFiles
-OUT_DIR=~/HEP_Project/GAPS_Project/nakagami/data_40M
+# OUT_DIR=~/HEP_Project/GAPS_Project/nakagami/data_40M  # test directory
+OUT_DIR=/mnt/ynakagami3/nakagami_data/data_40M # real directory
 SCRIPT=~/HEP_Project/GAPS_Project/nakagami/data_parse/preprocess_40M.py
 LOG=~/HEP_Project/GAPS_Project/nakagami/bash/preprocess_40M.log
 
